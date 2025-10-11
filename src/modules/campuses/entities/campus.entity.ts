61f1dab905c1fef5e0375dc5923cb6b4a20abb8a
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-10-10 22:41:54 -0500

    feat: implement Campus entity with properties and database mapping

--- src/modules/campuses/entities/campus.entity.ts
+++ src/modules/campuses/entities/campus.entity.ts
@@ -1,1 +1,21 @@
-export class Campus {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'campuses',
+})
+export class Campus {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  city: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  address: string;
+
+  @Column({ type: 'boolean', default: true, name: 'is_active' })
+  isActive: boolean;
+}
