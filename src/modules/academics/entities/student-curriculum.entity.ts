9da2d5f21d47d06d4787fb0f92e66f294b4f3fa1
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-10-13 00:07:38 -0500

    feat: add RelationId properties for studentId and curriculumId in StudentCurriculum entity

--- src/modules/academics/entities/student-curriculum.entity.ts
+++ src/modules/academics/entities/student-curriculum.entity.ts
@@ -5,6 +5,7 @@ import {
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  RelationId,
 } from 'typeorm';
 import { Student } from './student.entity';
 import { Curriculum } from './curriculum.entity';
@@ -20,11 +21,17 @@ export class StudentCurriculum {
   @Column({ type: 'boolean', default: true, name: 'is_active' })
   isActive: boolean;
 
+  @RelationId((sc: StudentCurriculum) => sc.student)
+  studentId: string;
+
   @ManyToOne(() => Student, (student) => student.studentCurricula, {
     nullable: false,
   })
   @JoinColumn({ name: 'student_id' })
   student: Student;
+
+  @RelationId((sc: StudentCurriculum) => sc.curriculum)
+  curriculumId: number;
 
   @ManyToOne(() => Curriculum, (curriculum) => curriculum.studentCurricula, {
     nullable: false,
