6073c97c4d0265dfc23d756b2ce6999f230a51e7
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-11-09 18:23:39 -0500

    feat: implement global exception handling with AllExceptionsFilter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,19 +3,27 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './app/filters/all-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: () => {
+        return new BadRequestException({
+          message: 'Validation failed',
+        });
+      },
+    })
   );
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+void bootstrap();
