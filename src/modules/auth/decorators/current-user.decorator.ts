72fb3b85417bacf6b7832c3e3e9396a508d77204
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-11-24 01:26:37 -0500

    feat: add CurrentUser decorator and IUser interface for user context retrieval

---
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IUser } from '../interfaces/user.interface';
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof IUser | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (data) {
+      return user?.[data];
+    }
+
+    return user;
+  },
+);
---
+++ src/modules/auth/interfaces/user.interface.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  userId: string;
+  username: string;
+  email: string;
+  roles: string[];
+}
